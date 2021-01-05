7bdbd98099fcfc76127f6505529197a28e5517cf
Ilias El Abbassi <iliaselabbassi@outlook.fr>
2021-01-05 17:01:25 +0100

    added merkle tree implementation

--- block.py
+++ block.py
@@ -19,19 +19,59 @@ class Block:
         self.index = index
         self.previousHash = previousHash
         self.timestamp = timestamp
-        self.hash = ""
         self.nonce = 0
     
     '''
     Method to compute the block
     We concatenate the block information and hash it
     '''
-    def computeHash(self):
-        toHash = json.dumps(self.__dict__, sort_keys=True)
+    def computeHash(self, merkle=False):
+        if not merkle:
+            toHash = json.dumps(self.__dict__, sort_keys=True)
+            return hashlib.sha256(toHash.encode()).hexdigest()
+        else:
+            return self.merkleTree()
+            pass
+        
+    '''
+    merkle tree method init
+    '''
+    def merkleTree(self):
+        data = self.__dict__
+        merkleBlocks = []
+        
+        for ele in data:
+            merkleBlocks.append(json.dumps(str(ele)+"="+str(data[ele])))
+        
+        if len(merkleBlocks) % 2 != 0:
+            merkleBlocks.append(json.dumps("Fill block"))
+           
+        for i in range(0, len(merkleBlocks)):
+            merkleBlocks[i] = hashlib.sha256(merkleBlocks[i].encode()).hexdigest()
+            
+        return self.merkleTreeRecursion(merkleBlocks)
 
-        return hashlib.sha256(toHash.encode()).hexdigest()
+    '''
+    Recursion method used in merkleTree method
+    '''
+    def merkleTreeRecursion(self, blocks):
+            temp = []
+            if len(blocks) != 1:
+                if len(blocks) % 2 != 1:
+                    for i in range(0, len(blocks), 2):
+                        temp.append(hashlib.sha256(json.dumps(blocks[i]+blocks[i+1]).encode()).hexdigest())
+                    return self.merkleTreeRecursion(temp)
+                else:
+                    blocks.append("filling block")
+                    for i in range(0, len(blocks), 2):
+                        temp.append(hashlib.sha256(json.dumps(blocks[i]+blocks[i+1]).encode()).hexdigest())
+                    return self.merkleTreeRecursion(temp)
 
+            return hashlib.sha256(blocks[len(blocks)-1].encode()).hexdigest()
+        
+        
 if __name__ == "__main__":
-    print("Red velvet")
-    b = Block(1,"red velvet","abcdeer", 1234)
-    print(b.__dict__)
+    print("Red velvet\n")
+    b = Block(1,":)","abcdeer", 1234)
+    
+    a = b.merkleTree()
--- blockchain.py
+++ blockchain.py
@@ -7,7 +7,7 @@ Main file to modelise a blockchain
 '''
 
 class Blockchain:
-    difficulty = 5
+    difficulty = 3
     '''
     Blockchain class constructor
     '''
@@ -22,22 +22,23 @@ class Blockchain:
     '''
     def generateGenesisBlock(self):
         genesisBlock = block.Block(0, "I am the Genesis Block :)", "Asseul", time.time())
-        genesisBlock.hash = genesisBlock.computeHash()
+        genesisBlock.hash = genesisBlock.computeHash(merkle=True)
         self.chain.append(genesisBlock)
 
     '''
     check if a block can be added to the chain and add it
     '''
     def add_block(self, block, proof):
-
         previousHash = self.chain[-1].hash
-
+        
         if previousHash != block.previousHash:
+            print("previous hash error")
             return False
 
         if not self.isValid(block, proof):
-            return Fakse
-
+            print("validity error")
+            return False
+        
         block.hash = proof
         self.chain.append(block)
 
@@ -47,13 +48,13 @@ class Blockchain:
     Find the nonce on of block
     '''
     def proofOfWork(self, block):
-        block.nonce = 0
-
-        computedHash = block.computeHash()
-
+        block.nonce = 0 
+        computedHash = block.computeHash(merkle=True)
+        
         while not computedHash.startswith('0'*Blockchain.difficulty):
             block.nonce += 1
-            computedHash = block.computeHash()
+            computedHash = block.computeHash(merkle=True)
+            
         
         return computedHash
 
@@ -63,7 +64,7 @@ class Blockchain:
     and if we re-compute the hash we obtain the exact same hash
     '''
     def isValid(self, block, blockHash):
-        return (blockHash.startswith('0'*Blockchain.difficulty) and blockHash == block.computeHash())
+        return (blockHash.startswith('0'*Blockchain.difficulty) and blockHash == block.computeHash(merkle=True))
     
     """
     Check if the blockchain is valid or not thanks to the cryptographic function
@@ -75,7 +76,7 @@ class Blockchain:
             previous_hash = self.chain[i].hash
             current_hash = self.chain[i+1].previousHash
             
-            print("previous : "+previous_hash + "  new : "+ current_hash)
+            print("previous : "+previous_hash + "  current : "+ current_hash)
             
             if not previous_hash == current_hash:
                 print("\n=================")
