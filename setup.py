d8082a966f828e00ad9c70f37b23a6510259d9af
Ilias El Abbassi <ilias.elabbassi.etu@univ-lille.fr>
2022-01-25 13:12:08 +0100

    feat: created setup.py and pyproject.toml files

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    include_package_data=True,
+    name="TREE_FTP",
+    version="0.0.1",
+    description="FTP Client Server for SR1 TP",
+    author="Ilias El Abbassi",
+    author_email="ilias.elabbassi.etu@univ-lille.fr",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "Flask"
+    ]
+)
